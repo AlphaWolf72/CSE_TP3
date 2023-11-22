5629ccc0cbdc429b95e45b61b94adc21ab0c2f52
tsinglande <thomas.singlande@etudiant.univ-rennes.fr>
2023-11-22 09:46:52 +0100

    Modification du semaphore à terminer

--- src/AirSimulation.java
+++ src/AirSimulation.java
@@ -16,9 +16,10 @@ public class AirSimulation
    private int nAgent2;
    private int nAgent3;
    private int nAgent4;
-   private Aircraft a;
-   private Semaphore sem = new Semaphore(1);
-   public final int nagents = 4;
+   private final Aircraft a;
+   private static final Semaphore sem = new Semaphore(1);
+
+   private static Semaphore[][] sem_tab;
 
    // Constructor
    public AirSimulation()
@@ -28,12 +29,7 @@ public class AirSimulation
       this.nAgent3 = 0;
       this.nAgent4 = 0;
       this.a = new Aircraft();  // standard model
-   }
-
-   // Reference to Aircraft
-   public Aircraft getAircraftRef()
-   {
-      return this.a;
+      sem_tab = new Semaphore[this.a.getNumberOfRows()][this.a.getSeatsPerRow()];
    }
 
    // Agent1
@@ -52,6 +48,7 @@ public class AirSimulation
          int row = R.nextInt(this.a.getNumberOfRows());
          int col = R.nextInt(this.a.getSeatsPerRow());
 
+         sem_tab[row][col].acquire();
          // verifying whether the seat is free
          if (this.a.isSeatEmpty(row,col))
          {
@@ -62,6 +59,7 @@ public class AirSimulation
                placed = true;
             }
          }
+         sem_tab[row][col].release();
       }
       while (!placed && !this.a.isFlightFull());
 
@@ -85,9 +83,11 @@ public class AirSimulation
          int col = 0;
          while (!placed && col < this.a.getSeatsPerRow())
          {
+            sem_tab[row][col].acquire();
             // verifying whether the seat is free
             if (this.a.isSeatEmpty(row,col))
             {
+
                // if this is an emergency exit seat, and c needs assistance, then we skip
                if (!emergRows.contains(row) || !c.needsAssistence() || this.a.numberOfFreeSeats() <= this.a.getSeatsPerRow() * this.a.getNumberEmergencyRows())
                {
@@ -95,6 +95,7 @@ public class AirSimulation
                   placed = true;
                }
             }
+            sem_tab[row][col].release();
             col++;
          }
          row++;
@@ -109,26 +110,47 @@ public class AirSimulation
    {
       Random R = new Random();
 
-      int row = 0;
-      int col = 0;
-      int row2 = 0;
-      int col2 = 0;
+      int row = R.nextInt(this.a.getNumberOfRows());
+      int col = R.nextInt(this.a.getSeatsPerRow());
+      int row2 = R.nextInt(this.a.getNumberOfRows());
+      int col2 = R.nextInt(this.a.getSeatsPerRow());
+
+      sem_tab[row][col].acquire();
+
+      int i = 0;
 
       // randomly pick a seat
-      while (this.a.isSeatEmpty(row,col) || this.a.isSeatEmpty(row2,col2))
+      while (this.a.isSeatEmpty(row,col))
       {
+         sem_tab[row][col].release();
+
          row = R.nextInt(this.a.getNumberOfRows());
          col = R.nextInt(this.a.getSeatsPerRow());
+
+         sem_tab[row][col].acquire();
+         System.out.println(i++);
+      }
+
+      sem_tab[row2][col2].acquire();
+      // randomly pick a seat
+      while (this.a.isSeatEmpty(row2,col2) || row == row2 && col == col2)
+      {
+         if (row != row2 || col != col2)
+            sem_tab[row][col].release();
          row2 = R.nextInt(this.a.getNumberOfRows());
          col2 = R.nextInt(this.a.getSeatsPerRow());
+         if (row != row2 || col != col2)
+            sem_tab[row2][col2].acquire();
+
+
+         System.out.println(i++);
       }
 
       // generating a new Customer
       Customer c = this.a.getCustomer(row,col);
       Customer c2 = this.a.getCustomer(row2,col2);
-
       // checking frequent flyer numbers
-      if (c.getFlyerLevel() > c2.getFlyerLevel())
+      if ((c.getFlyerLevel() > c2.getFlyerLevel() && row > row2) || (c.getFlyerLevel() < c2.getFlyerLevel() && row > row2))
       {
          this.a.freeSeat(row,col);
          this.a.freeSeat(row2,col2);
@@ -136,6 +158,8 @@ public class AirSimulation
          this.a.add(c2,row,col);
       }
       // updating counter
+      sem_tab[row][col].release();
+      sem_tab[row2][col2].release();
       this.nAgent3++;
    }
 
@@ -146,9 +170,11 @@ public class AirSimulation
       {
          for (int j = 0; j < this.a.getSeatsPerRow(); j++)
          {
+            //sem_tab[i][j].acquire();
             Customer c = this.a.getCustomer(i,j);
             this.a.freeSeat(i,j);
             if (c != null) this.a.add(c,i,j);
+            //sem_tab[i][j].release();
          }
       }
       this.nAgent4++;
@@ -169,74 +195,8 @@ public class AirSimulation
    {
       String print = "AirSimulation (agent1 : " + this.nAgent1 + ", agent2 : " + this.nAgent2 + ", " +
               "agent3 : " + this.nAgent3 + ", agent4 : " + this.nAgent4 + ")\n";
-      print = print + a.toString();
+      print = print + a;
       return print;
-   }
-
-   class Agent extends Thread{
-
-      private Thread t;
-      private String nameAgent;
-      private AirSimulation s;
-
-      public Agent(String nameAgent, AirSimulation s){
-         this.nameAgent = nameAgent;
-         this.s = s;
-      }
-
-      @Override
-      public void run(){
-         switch(nameAgent){
-            case "Agent2":
-               while (!s.getAircraftRef().isFlightFull()){
-                  try{
-                     s.sem.acquire();
-                     try{
-                        s.agent2();
-                     }finally{
-                        s.sem.release();
-                     }
-                  }catch(InterruptedException e){
-                     e.printStackTrace();
-                  }
-               }
-               break;
-            case "Agent3":
-               while (!s.getAircraftRef().isFlightFull()){
-                  try{
-                     s.sem.acquire();
-                     try{
-                        s.agent3();
-                     }finally{
-                        s.sem.release();
-                     }
-                  }catch(InterruptedException e){
-                     e.printStackTrace();
-                  }
-               }
-               break;
-            case "Agent4":
-               while (!s.getAircraftRef().isFlightFull()){
-                  try{
-                     s.sem.acquire();
-                     try{
-                        s.agent4();
-                     }finally{
-                        s.sem.release();
-                     }
-                  }catch(InterruptedException e){
-                     e.printStackTrace();
-                  }
-               }break;
-         }
-      }
-
-      public void start(){
-         if(t == null){
-            t = new Thread(this, nameAgent);
-            t.start();
-         }
-      }
    }
 
    // Simulation in parallel (main)
@@ -244,93 +204,83 @@ public class AirSimulation
    {
       System.out.println("\n** Sequential execution **\n");
       long startSequentitalExec = System.currentTimeMillis();
-      if (args != null && args.length > 0 && args[0] != null && args[0].equals("animation"))
+      AirSimulation s = new AirSimulation();
+      for(int i = 0; i < s.a.getNumberOfRows(); i++){
+         for(int j = 0; j < s.a.getSeatsPerRow() ; j++){
+            sem_tab[i][j] = new Semaphore(1);
+         }
+      }
+      while (!s.a.isFlightFull())
       {
-         AirSimulation s = new AirSimulation();
-         while (!s.a.isFlightFull())
+         s.agent1();
+         s.agent2();
+         s.agent3();
+         s.agent4();
+         if (args != null && args.length > 0 && args[0] != null && args[0].equals("animation"))
          {
-            s.agent1();
-            s.agent2();
-            s.agent3();
-            s.agent4();
             System.out.println(s + s.a.cleanString());
             Thread.sleep(100);
          }
-         System.out.println(s);
       }
-      else
-      {
-         AirSimulation s = new AirSimulation();
-         while (!s.a.isFlightFull())
-         {
-            s.agent1();
-            s.agent2();
-            s.agent3();
-            s.agent4();
-         }
-         System.out.println(s);
-      }
+      System.out.println(s);
+
       long endSequentialExec = System.currentTimeMillis();
       long sequentialExec = endSequentialExec - startSequentitalExec;
-      System.out.println("Sequential execution time: " + sequentialExec);
+      s.reset();
+
+      System.out.println("Sequential execution time: " + sequentialExec + "ms");
 
       System.out.println("\n** Parallel execution **\n");
       long startParallelExec = System.currentTimeMillis();
-      if (args != null && args.length > 0 && args[0] != null && args[0].equals("animation"))
-      {
-         AirSimulation s = new AirSimulation();
-         Agent agent2 = s.new Agent("Agent2", s);
-         Agent agent3 = s.new Agent("Agent3", s);
-         Agent agent4 = s.new Agent("Agent4", s);
-         agent2.start();
-         agent3.start();
-         agent4.start();
-         while (!s.getAircraftRef().isFlightFull())
-         {
-            try{
-               s.sem.acquire();
-               try{
-                  s.agent1();
-               }finally{
-                  s.sem.release();
-               }
-            }catch(InterruptedException e){
-               e.printStackTrace();
+      Thread agent2 = new Thread(() -> {
+         while (!s.a.isFlightFull()){
+            try {
+               s.agent2();
+            } catch (InterruptedException e) {
+               throw new RuntimeException(e);
             }
-            System.out.println(s + s.getAircraftRef().cleanString());
+         }
+      });
+
+      Thread agent3 = new Thread(() -> {
+         while (!s.a.isFlightFull()){
+            try {
+               s.agent3();
+            } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+            }
+         }
+      });
+
+      Thread agent4 = new Thread(() -> {
+         while (!s.a.isFlightFull()){
+            try {
+               s.agent4();
+            } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+            }
+         }
+      });
+
+      agent2.start();
+      agent3.start();
+      agent4.start();
+
+      while (!s.a.isFlightFull())
+      {
+         s.agent1();
+         if (args != null && args.length > 0 && args[0] != null && args[0].equals("animation"))
+         {
+            System.out.println(s + s.a.cleanString());
             Thread.sleep(100);
          }
-         System.out.println(s);
       }
-      else
-      {
-         AirSimulation s = new AirSimulation();
-         Agent agent2 = s.new Agent("Agent2", s);
-         Agent agent3 = s.new Agent("Agent3", s);
-         Agent agent4 = s.new Agent("Agent4", s);
+      System.out.println(s);
 
-         agent2.start();
-         agent3.start();
-         agent4.start();
-         while (!s.getAircraftRef().isFlightFull())
-         {
-            try{
-               s.sem.acquire();
-               try{
-                  s.agent1();
-               }finally{
-                  s.sem.release();
-               }
-            }catch(InterruptedException e){
-               e.printStackTrace();
-            }
-         }
-         System.out.println(s);
-      }
       long endParallelExec = System.currentTimeMillis();
       long parallelExec = endParallelExec - startParallelExec;
 
-      System.out.println("Parallel execution time: " + parallelExec);
+      System.out.println("Parallel execution time: " + parallelExec + "ms");
    }
 }
 
